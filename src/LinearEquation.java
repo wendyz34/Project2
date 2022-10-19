169ccf1d00328b5076b332ae47277cd8fb0789ce
wendy <wendyz34@nycstudents.net>
2022-10-19 18:55:27 -0400

    print

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -5,62 +5,67 @@ public class LinearEquation {
     private int y2;
 
     public LinearEquation(int x1, int y1, int x2, int y2) {
-        this.x1=x1;
-        this.y2=x2;
-        this.x2=x2;
-        this.y2=y2;
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
         if (x1 != x2) {
-            String coord1 = "( " + x1 +","+ y1 + ")";
-            String coord2 = "( " + x2 +","+ y2 + ")";
-        }else{
-            double slopeXsame = 1;
+            String coord1 = "( " + x1 + "," + y1 + ")";
+            String coord2 = "( " + x2 + "," + y2 + ")";
         }
+
+
 
 
     }
     public double distance(){
-        double x = (x2-x1)^2;
-        double y =(y2-y1)^2;
+        double x = Math.pow((x2-x1),2);
+        double y = Math.pow((y2-y1),2);
         double dis = Math.sqrt(x+y);
-        System.out.println(x);
-        System.out.println(y);
-
-
-
-        return Math.round((100*dis))/100;
+        return roundedToHundredth(dis);
 
 
 
 
     }
     public double yIntercept(){
-        double yy = (y2-y1);
-        double xx = (x2-x1)*2;
-        double yInter= y2/(yy/xx);
-        return Math.round(yInter);
+        double ySlope=(y2-y1);
+        double xSlope=(x2-x1);
+        double slope = roundedToHundredth(ySlope/xSlope);
+        double yInter= y2-(slope*x2);
+        return roundedToHundredth(yInter);
 
     }
     public double slope(){
-        double slope = (y2-y1)/(x2-x1);
-        return Math.round(slope);
+        double ySlope=(y2-y1);
+        double xSlope=(x2-x1);
+        double slope = roundedToHundredth(ySlope/xSlope);
+        return slope;
     }
     public String equation(){
-
         String equation = "y = " + slope() + "x + " + yIntercept();
         return equation;
     }
     public String coordinateForX(double xValue){
-        double coordY = xValue * slope() +yIntercept();
+        double newY = roundedToHundredth(xValue) * slope() +yIntercept();
+        double coordY = roundedToHundredth(newY);
         String newCoord = "(" + xValue + "," + coordY + ")";
         return newCoord;
 
 
     }
     public double roundedToHundredth(double toRound){
-        return Math.round(toRound);
+        return Math.round(toRound*100)/100.000;
 
     }
     public String lineInfo(){
+        System.out.println("These two points are: " + "( " + x1 + "," + y1 + ")" +"and" + "( " + x2 + "," + y2 + ")");
+        System.out.println("The equation between these two points is: " + equation());
+        System.out.println("The slope of this line is: " + slope());
+        System.out.println("The y-intercept of the line is: " + yIntercept());
+        System.out.println("The distance between the two points is: " + distance());
+        String line = "";
+        return line;
 
     }
 
