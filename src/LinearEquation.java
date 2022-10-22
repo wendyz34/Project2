63b51941ede34d1652ab77781f21d429952b17be
wendy <wendyz34@nycstudents.net>
2022-10-21 21:14:24 -0400

    print

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -4,7 +4,7 @@ public class LinearEquation {
     private int x2;
     private int y2;
 
-    public LinearEquation(int x1, int y1, int x2, int y2) {
+    public LinearEquation(int x1,int y1, int x2, int y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -13,9 +13,9 @@ public class LinearEquationRunner {
         System.out.println("Enter coordinate 2: ");
         String coord2 = scan.nextLine();
         int x1 = Integer.parseInt(coord1.substring(1,coord2.indexOf(",")));
-        int y1= Integer.parseInt(coord1.substring(coord1.indexOf(" ,")+1 , coord1.indexOf(")")));
+        int y1= Integer.parseInt(coord1.substring(coord1.indexOf(" ")+1 , coord1.indexOf(")")));
         int x2= Integer.parseInt(coord2.substring(1,coord2.indexOf(",")));
-        int y2= Integer.parseInt(coord2.substring(coord2.indexOf(", ")+1 ,coord2.indexOf(")")));
+        int y2= Integer.parseInt(coord2.substring(coord2.indexOf(" ")+1 ,coord2.indexOf(")")));
         LinearEquation obj= new LinearEquation(x1,y1,x2,y2);
         System.out.println(obj.start());
         System.out.println("Equation: " + obj.equation());
